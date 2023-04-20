9b3c6459b59e3d687772c940625073cc298320d8
Ian Sanders <iansan5653@github.com>
2023-04-20 19:04:03 -0400

    Add extension manifest and observe for GitHub editors

---
+++ src/observe.js
@@ -0,0 +1,31 @@
+/**
+ * Watch the document DOM for the selector.
+ * @param {string} selector
+ * @param {(element: HTMLElement) => () => void} onAdd
+ */
+export function observeSelector(selector, onAdd) {
+  const parent = document.body
+
+  let removeHandlers = new Map()
+   
+  for (const element of parent.querySelectorAll(selector)) removeHandlers.set(element, onAdd(element))
+
+  const observer = new MutationObserver(() => {
+    const found = new Set()
+    for (const element of parent.querySelectorAll(selector)) {
+      found.add(element)
+
+      if (!removeHandlers.has(element)) removeHandlers.set(element, onAdd(element))
+    }
+
+    for (const [element, onRemove] of removeHandlers.entries()) if (!found.has(element)) {
+      onRemove()
+      removeHandlers.delete(element)
+    }
+  })
+
+  observer.observe(parent, {
+    childList: true,
+    subtree: true
+  })
+}
--- src/scripts.js
+++ src/scripts.js
@@ -5,9 +5,12 @@
 import markdownlint from "markdownlint";
 import markdownlintGitHub from "@github/markdownlint-github";
 import {getCharacterCoordinates} from "./character-coordinates";
+import {observeSelector} from "./observe";
+
+console.log("big deal")
 
 /** @param {string} markdown */
-const lint = (markdown) => 
+const lintString = (markdown) => 
   markdownlint.sync({
     strings: {
       content: markdown,
@@ -17,37 +20,35 @@ const lint = (markdown) =>
     customRules: markdownlintGitHub,
   }).content
 
-
-const textarea = /** @type {HTMLTextAreaElement} */ (document.getElementById('demo-target'))
-const portal = document.getElementById('portal');
-
-(() => {
-  if (!portal || !textarea) throw new Error("missing textarea or portal target element")
-
-  const markdown = textarea.value
-  const errors = lint(markdown)
+/**
+ * @param {HTMLTextAreaElement} editor
+ * @param {HTMLElement} portal
+ */
+const lintEditor = (editor, portal) => {
+  const markdown = editor.value
+  const errors = lintString(markdown)
 
   for (const annotation of portal.children) portal.removeChild(annotation)
 
   const lines = markdown.split('\n')
-  for (const {lineNumber, errorRange, ruleDescription} of errors) {
+  for (const {lineNumber, errorRange} of errors) {
     const [line, ...prevLines] = lines.slice(0, lineNumber).reverse()
     const prevLineChars = prevLines.reduce((t, l) => t + l.length + 1 /* add one for newline char */, 0)
     const lineStart = errorRange?.[0] ?? 0
     const startIndex = prevLineChars + (errorRange?.[0] ?? 1) - 1
-    const startCoords = getCharacterCoordinates(textarea, startIndex)
+    const startCoords = getCharacterCoordinates(editor, startIndex)
     
     let endIndex = startIndex + (errorRange?.[1] ?? (line.length - lineStart + 1)) - 1
-    let endCoords = getCharacterCoordinates(textarea, endIndex)
+    let endCoords = getCharacterCoordinates(editor, endIndex)
     while (endCoords.height > startCoords.height) {
-      endCoords = getCharacterCoordinates(textarea, --endIndex)
+      endCoords = getCharacterCoordinates(editor, --endIndex)
     }
 
     console.log(startIndex, endIndex, markdown.slice(startIndex, endIndex + 1))
 
     const annotation = document.createElement('span')
     annotation.style.position = 'absolute'
-    annotation.style.top = `${startCoords.top}px`
+    annotation.style.top = `${startCoords.top - 2}px`
     annotation.style.left = `${startCoords.left}px`
     annotation.style.width = `${endCoords.left - startCoords.left}px`
     annotation.style.borderBottom = '2px dashed red'
@@ -55,4 +56,27 @@ const portal = document.getElementById('portal');
     annotation.style.pointerEvents = 'none'
     portal.appendChild(annotation)
   }
-})()
+}
+
+const markdownEditorsSelector = 'textarea.js-paste-markdown, textarea.CommentBox-input'
+
+observeSelector(markdownEditorsSelector, editor => {
+  const portal = document.createElement('div')
+  document.body.appendChild(portal)
+
+  const refreshLint = () => lintEditor(/** @type {HTMLTextAreaElement} */(editor), portal)
+  
+  refreshLint()
+
+  document.addEventListener('input', refreshLint)
+
+  const resizeObserver = new ResizeObserver(refreshLint)
+  resizeObserver.observe(editor)
+
+  return () => {
+    document.removeEventListener('input', refreshLint)
+    document.body.removeChild(portal)
+    resizeObserver.disconnect()
+  }
+})
+
