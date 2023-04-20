e155bca5d2ceb9a768f17a54836b287845206807
Ian Sanders <iansan5653@github.com>
2023-04-20 18:11:09 -0400

    Add ability to render inline error annotations

---
+++ src/character-coordinates.js
@@ -0,0 +1,172 @@
+// Note that some browsers, such as Firefox, do not concatenate properties
+// into their shorthand (e.g. padding-top, padding-bottom etc. -> padding),
+// so we have to list every single property explicitly.
+const propertiesToCopy = [
+  'direction', // RTL support
+  'boxSizing',
+  'width', // on Chrome and IE, exclude the scrollbar, so the mirror div wraps exactly as the textarea does
+  'height',
+  'overflowX',
+  'overflowY', // copy the scrollbar for IE
+
+  'borderTopWidth',
+  'borderRightWidth',
+  'borderBottomWidth',
+  'borderLeftWidth',
+  'borderStyle',
+
+  'paddingTop',
+  'paddingRight',
+  'paddingBottom',
+  'paddingLeft',
+
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/font
+  'fontStyle',
+  'fontVariant',
+  'fontWeight',
+  'fontStretch',
+  'fontSize',
+  'fontSizeAdjust',
+  'lineHeight',
+  'fontFamily',
+
+  'textAlign',
+  'textTransform',
+  'textIndent',
+  'textDecoration', // might not make a difference, but better be safe
+
+  'letterSpacing',
+  'wordSpacing',
+
+  'tabSize',
+  'MozTabSize', // prefixed version for Firefox <= 52
+]
+
+
+/**
+ * Obtain the coordinates (px) of the top left of a character in an input, relative to
+ * the viewport.
+ *
+ * Forked from https://github.com/primer/react/blob/src/drafts/utils/character-coordinates.ts,
+ * which was forked from https://github.com/koddsson/textarea-caret-position, which was
+ * forked from https://github.com/component/textarea-caret-position.
+ *
+ * @param {HTMLTextAreaElement | HTMLInputElement} element The target input element.
+ * @param {number} index The index of the character to calculate.
+ */
+export function getCharacterCoordinates(element, index) {
+  const isFirefox = "mozInnerScreenX" in window;
+
+  // The mirror div will replicate the textarea's style
+  const div = document.createElement("div");
+  div.id = "input-textarea-caret-position-mirror-div";
+  document.body.appendChild(div);
+
+  const style = div.style;
+  const computed = window.getComputedStyle(element);
+
+  // Lineheight is either a number or the string 'normal'. In that case, fall back to a
+  // rough guess of 1.2 based on MDN: "Desktop browsers use a default value of roughly 1.2".
+  const lineHeight = isNaN(parseInt(computed.lineHeight))
+    ? parseInt(computed.fontSize) * 1.2
+    : parseInt(computed.lineHeight);
+
+  const isInput = element instanceof HTMLInputElement;
+
+  // Default wrapping styles
+  style.whiteSpace = isInput ? "nowrap" : "pre-wrap";
+  style.wordWrap = isInput ? "" : "break-word";
+
+  // Position off-screen
+  style.position = "absolute"; // required to return coordinates properly
+
+  // Transfer the element's properties to the div
+  for (const prop of propertiesToCopy) {
+    if (isInput && prop === "lineHeight") {
+      // Special case for <input>s because text is rendered centered and line height may be != height
+      if (computed.boxSizing === "border-box") {
+        const height = parseInt(computed.height);
+        const outerHeight =
+          parseInt(computed.paddingTop) +
+          parseInt(computed.paddingBottom) +
+          parseInt(computed.borderTopWidth) +
+          parseInt(computed.borderBottomWidth);
+        const targetHeight = outerHeight + lineHeight;
+
+        if (height > targetHeight) {
+          style.lineHeight = `${height - outerHeight}px`;
+        } else if (height === targetHeight) {
+          style.lineHeight = computed.lineHeight;
+        } else {
+          style.lineHeight = "0";
+        }
+      } else {
+        style.lineHeight = computed.height;
+      }
+    } else if (
+      !isInput &&
+      prop === "width" &&
+      computed.boxSizing === "border-box"
+    ) {
+      // With box-sizing: border-box we need to offset the size slightly inwards.  This small difference can compound
+      // greatly in long textareas with lots of wrapping, leading to very innacurate results if not accounted for.
+      // Firefox will return computed styles in floats, like `0.9px`, while chromium might return `1px` for the same element.
+      // Either way we use `parseFloat` to turn `0.9px` into `0.9` and `1px` into `1`
+      const totalBorderWidth =
+        parseFloat(computed.borderLeftWidth) +
+        parseFloat(computed.borderRightWidth);
+      // When a vertical scrollbar is present it shrinks the content. We need to account for this by using clientWidth
+      // instead of width in everything but Firefox. When we do that we also have to account for the border width.
+      const width = isFirefox
+        ? parseFloat(computed.width) - totalBorderWidth
+        : element.clientWidth + totalBorderWidth;
+      style.width = `${width}px`;
+    } else {
+      style[prop] = computed[prop];
+    }
+  }
+
+  if (isFirefox) {
+    // Firefox lies about the overflow property for textareas: https://bugzilla.mozilla.org/show_bug.cgi?id=984275
+    if (element.scrollHeight > parseInt(computed.height))
+      style.overflowY = "scroll";
+  } else {
+    style.overflow = "hidden"; // for Chrome to not render a scrollbar; IE keeps overflowY = 'scroll'
+  }
+
+  div.textContent = element.value.substring(0, index);
+
+  // The second special handling for input type="text" vs textarea:
+  // spaces need to be replaced with non-breaking spaces - http://stackoverflow.com/a/13402035/1269037
+  if (isInput) div.textContent = div.textContent.replace(/\s/g, "\u00a0");
+
+  const span = document.createElement("span");
+  // Wrapping must be replicated *exactly*, including when a long word gets
+  // onto the next line, with whitespace at the end of the line before (#7).
+  // The  *only* reliable way to do that is to copy the *entire* rest of the
+  // textarea's content into the <span> created at the caret position.
+  // For inputs, '.' is enough because there is no wrapping.
+  span.textContent = isInput ? "." : element.value.substring(index) || "."; // because a completely empty faux span doesn't render at all
+  div.appendChild(span);
+
+  const {top: viewportOffsetTop, left: viewportOffsetLeft} =
+    element.getBoundingClientRect();
+
+  const coordinates = {
+    top:
+      span.offsetTop +
+      parseInt(computed.borderTopWidth) -
+      element.scrollTop +
+      viewportOffsetTop,
+    left:
+      span.offsetLeft +
+      parseInt(computed.borderLeftWidth) -
+      element.scrollLeft +
+      viewportOffsetLeft,
+    height: lineHeight,
+  };
+
+  document.body.removeChild(div);
+
+  return coordinates;
+}
--- src/scripts.js
+++ src/scripts.js
@@ -4,8 +4,9 @@
 
 import markdownlint from "markdownlint";
 import markdownlintGitHub from "@github/markdownlint-github";
+import {getCharacterCoordinates} from "./character-coordinates";
 
-/** @param markdown {string} */
+/** @param {string} markdown */
 const lint = (markdown) => 
   markdownlint.sync({
     strings: {
@@ -15,3 +16,43 @@ const lint = (markdown) =>
     handleRuleFailures: true,
     customRules: markdownlintGitHub,
   }).content
+
+
+const textarea = /** @type {HTMLTextAreaElement} */ (document.getElementById('demo-target'))
+const portal = document.getElementById('portal')
+
+textarea?.addEventListener('input', () => {
+  if (!portal || !textarea) throw new Error("missing textarea or portal target element")
+
+  const markdown = textarea.value
+  const errors = lint(markdown)
+
+  for (const annotation of portal.children) portal.removeChild(annotation)
+
+  const lines = markdown.split('\n')
+  for (const {lineNumber, errorRange, ruleDescription} of errors) {
+    const [line, ...prevLines] = lines.slice(0, lineNumber).reverse()
+    const prevLineChars = prevLines.reduce((t, l) => t + l.length + 1 /* add one for newline char */, 0)
+    const lineStart = errorRange?.[0] ?? 0
+    const startIndex = prevLineChars + (errorRange?.[0] ?? 0)
+    const startCoords = getCharacterCoordinates(textarea, startIndex)
+    
+    let endIndex = startIndex + (errorRange?.[1] ?? (line.length - lineStart)) 
+    let endCoords = getCharacterCoordinates(textarea, endIndex)
+    while (endCoords.height > startCoords.height) {
+      endCoords = getCharacterCoordinates(textarea, --endIndex)
+    }
+
+    console.log(startIndex, endIndex, markdown.slice(startIndex, endIndex + 1))
+
+    const annotation = document.createElement('span')
+    annotation.style.position = 'absolute'
+    annotation.style.top = `${startCoords.top}px`
+    annotation.style.left = `${startCoords.left}px`
+    annotation.style.width = `${endCoords.left - startCoords.left}px`
+    annotation.style.borderBottom = '2px dashed red'
+    annotation.style.height = `${startCoords.height}px`
+    annotation.style.pointerEvents = 'none'
+    portal.appendChild(annotation)
+  }
+})
