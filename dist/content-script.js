aa7c8164fd9f86dc2860650d18afcfc535f91c7a
Ian Sanders <iansan5653@github.com>
2023-04-24 17:38:00 -0400

    Clean up `refreshStyles` method

--- dist/content-script.js
+++ dist/content-script.js
@@ -632,16 +632,11 @@ class TextareaRange {
   }
   #refreshStyles() {
     const style = this.#div.style;
-    const computed = window.getComputedStyle(this.#element);
-
-    // Lineheight is either a number or the string 'normal'. In that case, fall back to a
-    // rough guess of 1.2 based on MDN: "Desktop browsers use a default value of roughly 1.2".
-    const lineHeight = isNaN(parseInt(computed.lineHeight)) ? parseInt(computed.fontSize) * 1.2 : parseInt(computed.lineHeight);
-    const isInput = this.#element instanceof HTMLInputElement;
+    const textareaStyle = window.getComputedStyle(this.#element);
 
     // Default wrapping styles
-    style.whiteSpace = isInput ? "nowrap" : "pre-wrap";
-    style.wordWrap = isInput ? "" : "break-word";
+    style.whiteSpace = "pre-wrap";
+    style.wordWrap = "break-word";
 
     // Position off-screen
     style.position = "fixed";
@@ -650,40 +645,22 @@ class TextareaRange {
     const isFirefox = ("mozInnerScreenX" in window);
 
     // Transfer the element's properties to the div
-    for (const prop of propertiesToCopy) {
-      if (isInput && prop === "lineHeight") {
-        // Special case for <input>s because text is rendered centered and line height may be != height
-        if (computed.boxSizing === "border-box") {
-          const height = parseInt(computed.height);
-          const outerHeight = parseInt(computed.paddingTop) + parseInt(computed.paddingBottom) + parseInt(computed.borderTopWidth) + parseInt(computed.borderBottomWidth);
-          const targetHeight = outerHeight + lineHeight;
-          if (height > targetHeight) {
-            style.lineHeight = `${height - outerHeight}px`;
-          } else if (height === targetHeight) {
-            style.lineHeight = computed.lineHeight;
-          } else {
-            style.lineHeight = "0";
-          }
-        } else {
-          style.lineHeight = computed.height;
-        }
-      } else if (!isInput && prop === "width" && computed.boxSizing === "border-box") {
-        // With box-sizing: border-box we need to offset the size slightly inwards.  This small difference can compound
-        // greatly in long textareas with lots of wrapping, leading to very innacurate results if not accounted for.
-        // Firefox will return computed styles in floats, like `0.9px`, while chromium might return `1px` for the same element.
-        // Either way we use `parseFloat` to turn `0.9px` into `0.9` and `1px` into `1`
-        const totalBorderWidth = parseFloat(computed.borderLeftWidth) + parseFloat(computed.borderRightWidth);
-        // When a vertical scrollbar is present it shrinks the content. We need to account for this by using clientWidth
-        // instead of width in everything but Firefox. When we do that we also have to account for the border width.
-        const width = isFirefox ? parseFloat(computed.width) - totalBorderWidth : this.#element.clientWidth + totalBorderWidth;
-        style.width = `${width}px`;
-      } else {
-        style[prop] = computed[prop];
-      }
+    for (const prop of propertiesToCopy) if (prop === "width" && textareaStyle.boxSizing === "border-box") {
+      // With box-sizing: border-box we need to offset the size slightly inwards.  This small difference can compound
+      // greatly in long textareas with lots of wrapping, leading to very innacurate results if not accounted for.
+      // Firefox will return computed styles in floats, like `0.9px`, while chromium might return `1px` for the same element.
+      // Either way we use `parseFloat` to turn `0.9px` into `0.9` and `1px` into `1`
+      const totalBorderWidth = parseFloat(textareaStyle.borderLeftWidth) + parseFloat(textareaStyle.borderRightWidth);
+      // When a vertical scrollbar is present it shrinks the content. We need to account for this by using clientWidth
+      // instead of width in everything but Firefox. When we do that we also have to account for the border width.
+      const width = isFirefox ? parseFloat(textareaStyle.width) - totalBorderWidth : this.#element.clientWidth + totalBorderWidth;
+      style.width = `${width}px`;
+    } else {
+      style[prop] = textareaStyle[prop];
     }
     if (isFirefox) {
       // Firefox lies about the overflow property for textareas: https://bugzilla.mozilla.org/show_bug.cgi?id=984275
-      if (this.#element.scrollHeight > parseInt(computed.height)) style.overflowY = "scroll";
+      if (this.#element.scrollHeight > parseInt(textareaStyle.height)) style.overflowY = "scroll";
     } else {
       style.overflow = "hidden"; // for Chrome to not render a scrollbar; IE keeps overflowY = 'scroll'
     }
