e45afe695e25e1e1d10f717a6d1adbf87711d2f9
Ian Sanders <iansan5653@github.com>
2023-08-02 17:46:10 +0000

    Format

--- src/utilities/lint-markdown.ts
+++ src/utilities/lint-markdown.ts
@@ -17,8 +17,8 @@ export const lintMarkdown = (markdown: string): LintError[] =>
         "no-empty-links": true,
         // While enforcing a certain unordered list style can be somewhat helpful for making the Markdown source
         // easier to read with a screen reader, this rule is ultimately too opinionated and noisy to be worth it,
-        // especially because it conflicts with the editor's bulleted list toolbar button. 
-        "ul-style": false
+        // especially because it conflicts with the editor's bulleted list toolbar button.
+        "ul-style": false,
       }),
       handleRuleFailures: true,
       customRules: markdownlintGitHub,
